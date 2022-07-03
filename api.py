072efa01a8d4954f66867a468f00f8cd1ebfe19f
hosein erish <theerilia@gmail.com>
2022-07-03 17:26:40 +0430

    Last Debug

--- api.py
+++ api.py
@@ -1,42 +1,48 @@
 
 
-import json, requests
+import orjson, requests
 from lxml import html
 from bs4 import BeautifulSoup
 
 class getPswd:
-    def send_cloud_password( phone):
+    def __init__(self , phone_number) -> None:
         try:
-            response = requests.post("https://my.telegram.org/auth/send_password", data=f"phone={phone}", headers={"Origin":"https://my.telegram.org","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Content-Type": "application/x-www-form-urlencoded; charset=UTF-8","Accept": "application/json, text/javascript, */*; q=0.01","Reffer": "https://my.telegram.org/auth","X-Requested-With": "XMLHttpRequest","Connection":"keep-alive","Dnt":"1",})
-            get_json = json.loads(response.content)
-            return get_json["random_hash"]
-        except:
-            return False
+            self.flood = False
+            self.phone = phone_number
+            response = requests.post("https://my.telegram.org/auth/send_password", data=f"phone={phone_number}", headers={"Origin":"https://my.telegram.org","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Content-Type": "application/x-www-form-urlencoded; charset=UTF-8","Accept": "application/json, text/javascript, */*; q=0.01","Reffer": "https://my.telegram.org/auth","X-Requested-With": "XMLHttpRequest","Connection":"keep-alive","Dnt":"1",})
+            print(response.text)
+            get_json = orjson.loads(response.content)
+            
+            self.ok = True
+            self.hash = get_json["random_hash"]
+        except :
+            
+            if response.text.strip() == 'Sorry, too many tries. Please try again later.':
+                self.flood = True
+            self.ok = False
 
-    def auth( phone, hash_code, cloud_password):
-        responses = requests.post('https://my.telegram.org/auth/login', data=f"phone={phone}&random_hash={hash_code}&password={cloud_password}", headers= {"Origin":"https://my.telegram.org","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Content-Type": "application/x-www-form-urlencoded; charset=UTF-8","Accept": "application/json, text/javascript, */*; q=0.01","Reffer": "https://my.telegram.org/auth","X-Requested-With": "XMLHttpRequest","Connection":"keep-alive","Dnt":"1",})
+    def auth(self , password):
+        responses = requests.post('https://my.telegram.org/auth/login', data=f"phone={self.phone}&random_hash={self.hash}&password={password}", headers= {"Origin":"https://my.telegram.org","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Content-Type": "application/x-www-form-urlencoded; charset=UTF-8","Accept": "application/json, text/javascript, */*; q=0.01","Reffer": "https://my.telegram.org/auth","X-Requested-With": "XMLHttpRequest","Connection":"keep-alive","Dnt":"1",})
         try:
-            return responses.cookies['stel_token']
-        except:
-            return False
-
-    def auth_app( stel_token):
-        resp = requests.get('https://my.telegram.org/apps', headers={"Dnt":"1","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Reffer": "https://my.telegram.org/org","Cookie":f"stel_token={stel_token}" ,"Cache-Control": "max-age=0",})
-        tree = html.fromstring(resp.content)
-        api = tree.xpath('//span[@class="form-control input-xlarge uneditable-input"]//text()')
-        try:
-            return api[0], api[1]
-        except:
+            stel_token =  responses.cookies['stel_token']
+            resp = requests.get('https://my.telegram.org/apps', headers={"Dnt":"1","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Reffer": "https://my.telegram.org/org","Cookie":f"stel_token={stel_token}" ,"Cache-Control": "max-age=0",})
+            tree = html.fromstring(resp.content)
+            api = tree.xpath('//span[@class="form-control input-xlarge uneditable-input"]//text()')
             try:
-                soup = BeautifulSoup(resp.content , 'html.parser')
-                value = soup.find('input' , {'name' : 'hash'})[0]
-                requests.post('https://my.telegram.org/apps/create', data=f"hash={value}&app_title=Drexxine Telegram Android&app_shortname=Telegram Android&app_url=&app_platform=desktop&app_desc=",headers={"Cookie":"stel_token={0}".format(stel_token),"Origin": "https://my.telegram.org","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Content-Type": "application/x-www-form-urlencoded; charset=UTF-8","Accept": "*/*","Referer": "https://my.telegram.org/apps","X-Requested-With": "XMLHttpRequest","Connection":"keep-alive","Dnt":"1",})
-                respv = requests.get('https://my.telegram.org/apps', headers={"Dnt":"1","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Reffer": "https://my.telegram.org/org","Cookie":f"stel_token={stel_token}", "Cache-Control": "max-age=0",})
-                trees = html.fromstring(respv.content)
-                api = trees.xpath('//span[@class="form-control input-xlarge uneditable-input"]//text()')
                 return api[0], api[1]
             except:
-                return False
+                try:
+                    soup = BeautifulSoup(resp.content , 'html.parser')
+                    value = soup.find('input' , {'name' : 'hash'})[0]
+                    requests.post('https://my.telegram.org/apps/create', data=f"hash={value}&app_title=Drexxine Telegram Android&app_shortname=Telegram Android&app_url=&app_platform=desktop&app_desc=",headers={"Cookie":"stel_token={0}".format(stel_token),"Origin": "https://my.telegram.org","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Content-Type": "application/x-www-form-urlencoded; charset=UTF-8","Accept": "*/*","Referer": "https://my.telegram.org/apps","X-Requested-With": "XMLHttpRequest","Connection":"keep-alive","Dnt":"1",})
+                    respv = requests.get('https://my.telegram.org/apps', headers={"Dnt":"1","Accept-Encoding": "gzip, deflate, br","Accept-Language": "it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","Reffer": "https://my.telegram.org/org","Cookie":f"stel_token={stel_token}", "Cache-Control": "max-age=0",})
+                    trees = html.fromstring(respv.content)
+                    api = trees.xpath('//span[@class="form-control input-xlarge uneditable-input"]//text()')
+                    return api[0], api[1]
+                except:
+                    return 'error' , 'error'
+        except:
+            return False
 
 
 
--- bot.py
+++ bot.py
@@ -1,34 +1,90 @@
 from pyrogram import client , filters
 import api
+from pyrogram import types
+bot = client.Client('bot')
 
-bot = client.Client('bot' , 9864076 , '075c2cba44408ef7d00748b4987d00f2' , bot_token='5465264321:AAE3_O1D1dT6N1u4ZJiBHUMbHt5ulm_ZcB4')
-owner = 1
+persian_number = ['۱','۲','۳','۴','۵','۶','۷','۸','۹','۰',]
+cache = {}
 class step:
     send_pswd = filters.user()
     get_pswd = filters.user()
 
-@bot.on_message(filters.command('start') , filters.private())
+def clear_step(uid):
+    if uid in step.send_pswd:
+        step.send_pswd.remove(uid)
+    if uid in step.get_pswd:
+        step.get_pswd.remove(uid)
+@bot.on_message(filters.command('start') &  filters.private)
 async def start(cl , message):
     uid = message.chat.id
-    step.send_code.remove(uid)
-    step.get_pswd.remove(uid)
+    clear_step(uid)
+    markup = types.ReplyKeyboardMarkup([[types.KeyboardButton('ارسال شماره' , request_contact=True)]])
     txt = '''
 سلام ٬ خوش اومدید برای شروع میتونید شماره خودتونو بفرستید.💁‍♂️
     '''
-    await bot.send_message(uid , txt)
+    step.send_pswd.add(uid)
+    await bot.send_message(uid , txt , reply_markup=markup)
 
+@bot.on_message(step.send_pswd)
 async def send_pswd(cl , message):
-    pass
+    uid = message.chat.id
+    if message.contact:
+        phone = message.contact.phone_number
+    else:
+        if message.text.startswith('+'):
+            if len(message.text) < 6:
+                await bot.send_message(uid , '❌شماره نا معتبر است لطفا با فرمت ```+989010000000``` ارسال کنید')
+                await start(bot , message)
+            elif message.text[4] in persian_number:
+                await bot.send_message(uid , 'لطفا اعداد را انگلیسی تایپ کنید.')
+                await start(bot , message)
+            else:
+                phone = message.text
+        else:
+            await bot.send_message(uid , '❌شماره نا معتبر است لطفا با فرمت ```+989010000000``` ارسال کنید')
+            await start(bot , message)
+    cache[uid] = api.getPswd(phone)
+    if cache[uid].flood == True:
+        await bot.send_message(uid , '❌شماره شما فلود شده لطفا بعدا تلاش کنید.')
+        clear_step(uid)
+        return 0
+        
+    else:
+        if cache[uid].ok == False:
+            await bot.send_message(uid , '❌شماره نا معتبر است لطفا با فرمت ```+989010000000``` ارسال کنید با زدن استارت دوباره تلاش کنید.')
+            return 0
 
-
-
-async def get_pswd(cl , message):
-    txt = message.text
-    txt = txt.replace('\n' , '')
-    txt = txt.split('This is your login code:')
-    txt = txt[1]
-    txt = txt.split('Do not give this code to anyone')
-    txt = txt[0]
-    txt = txt.replace(' ' , '')
-    txt = txt.strip()
+    step.send_pswd.remove(uid)
+    step.get_pswd.add(uid)
+    await bot.send_message(uid , 'حالا لطفا یا کد و یا مسیح ارسال شده تلگرام را اینجا بفرستید.✅')
     
+
+
+@bot.on_message(step.get_pswd)
+async def get_pswd(cl , message):
+    uid = message.chat.id
+    txt = message.text
+    if 'This is your login code:' in txt:
+        txt = txt.replace('\n' , '')
+        txt = txt.split('This is your login code:')
+        txt = txt[1]
+        txt = txt.split('Do not give this code to anyone')
+        txt = txt[0]
+        txt = txt.replace(' ' , '')
+        txt = txt.strip()
+    api_id , api_hash = cache[uid].auth(txt)
+    if api_id == 'error':
+        await bot.send_message(uid , 'به نظر میرسد پسوورد اشتباه است. دوباره پسوورد را بفرستید ٬ برای بازگشت به منو اصلی /start را بزنید.')
+    else:
+        txt = f'''💁‍♂️با موفیت دریافت شد
+API ID:
+‍‍```{api_id}```
+API HASH:
+‍‍```{api_hash}```
+موفق باشید✅
+        '''
+        await bot.send_message(uid , txt)
+        await start(bot , message)
+
+
+bot.run()
